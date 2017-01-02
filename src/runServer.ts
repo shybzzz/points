f167b5bfaa2b04fd05f8a3b25a6d76b68b4c9456
shybzzz <rostykrom@yahoo.com>
2017-01-02 09:24:19 +0200

    mongoose is introduced to the project
    + better typings

--- src/runServer.ts
+++ src/runServer.ts
@@ -1,5 +1,6 @@
+import * as mongoose from "mongoose";
 let express = require("express");
-let applicationConfig = require("./application.config.json");
+let config = require("./application.config.json");
 
 let router = express.Router();
 router.get('/', function (req, resp) {
@@ -8,9 +9,11 @@ router.get('/', function (req, resp) {
     });
 });
 
+mongoose.connect(config.dbDest, config.mongoOptions);
+
 var app = express();
 app.use("/api", router);
-var server = app.listen(applicationConfig.port, function () {
+var server = app.listen(config.port, ()=> {
     var address = server.address();
     console.log("http://%s:%s", address.address, address.port);
 });
